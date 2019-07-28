220f2b21f31df64c7ea6033b33b607c2373c8ff4
Harry Muir <harrymuir@ITs-MacBook-Pro-2.local>
2019-07-28 10:53:42 -0600

    Updates

---
+++ precache-manifest.beec2305e05590933eaf2a2606075d5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad818a128f1cc07c29012534331883d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d62cfa90decb91ecba18",
+    "url": "/monsters-rolodex/static/css/main.aea4a850.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d62cfa90decb91ecba18",
+    "url": "/monsters-rolodex/static/js/main.7aa50497.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
